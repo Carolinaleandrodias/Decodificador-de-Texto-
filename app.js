30feab48773338079e2e55aa30a1e462e096466a
Carolinaleandrodias <carolinaleandrdias1999@gmail.com>
2024-07-01 22:28:43 -0300

    html e css prontos

---
+++ app.js
@@ -0,0 +1,8 @@
+let e = "enter";
+let i = "imes";
+let a = "ai";
+let o = "ober";
+let u = "ufat";
+
+//percorrer o valor de imput e ir salvando em uma variavel para cada leta concatenar o valor correspondente
+// quando clicar no botao o display vai mudar sua propriedadade para none e colocar o valor criptografado
