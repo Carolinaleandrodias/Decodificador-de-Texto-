2be10b14aa7e4e79efe570342e40ad92bf3c9321
Carolinaleandrodias <carolinaleandrdias1999@gmail.com>
2024-07-02 22:04:39 -0300

    atualizacao app.js

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-let e = "enter";
-let i = "imes";
-let a = "ai";
-let o = "ober";
-let u = "ufat";
+document.addEventListener('DOMContentLoaded', function() {	
+    let button = document.getElementById('criptografar');
+    let mensagem = document.getElementById('mensagem');
+
+    button.addEventListener('click', function() {
+        mensagem.textContent = criptografar(mensagem.value);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //percorrer o valor de imput e ir salvando em uma variavel para cada leta concatenar o valor correspondente
 // quando clicar no botao o display vai mudar sua propriedadade para none e colocar o valor criptografado
